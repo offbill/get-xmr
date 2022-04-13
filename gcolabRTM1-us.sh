c63afb7c908fa80be183d15e1789a1f79dba8e26
offbill <102901989+offbill@users.noreply.github.com>
2022-04-13 11:03:16 -0500

    Update gcolabRTM1-us.sh

--- gcolabRTM1-us.sh
+++ gcolabRTM1-us.sh
@@ -8,6 +8,6 @@ sudo apt install screen
 screen -R xmr
  
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-focal-x64.tar.gz
-tar xvzf xmrig-6.17.0-focal-x64.tar.gz
+tar xvf xmrig-6.17.0-focal-x64.tar.gz
 cd xmrig-6.17.0
 ./xmrig --donate-level 0 -a gr -o pool.minafacil.com:5555 --tls -u REEJjHmaqUjckCpbskcbgPpsrFcZiENNHD -p x
