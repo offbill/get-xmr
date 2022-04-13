08d91bf3e3c082fac477d489950a7e328564e89b
offbill <102901989+offbill@users.noreply.github.com>
2022-04-13 12:52:51 -0500

    Update gcolabRTM-us.sh

--- gcolabRTM-us.sh
+++ gcolabRTM-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig --donate-level 0 -a gr -o pool.minafacil.com:5555 --tls -u REEJjHmaqUjckCpbskcbgPpsrFcZiENNHD -p x
+./xmrig --donate-level 0 -a gr -o pool.minafacil.com:5555 --tls -u RWSuJqfFqMWdfPwnbLA7iLza3tAdDcc6g3.M18 -p x
