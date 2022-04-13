3899aa1200f219f532189aa9e75a1ae84784d2bc
offbill <102901989+offbill@users.noreply.github.com>
2022-04-13 10:45:44 -0500

    Update gcolabRTM1-us.sh

--- gcolabRTM1-us.sh
+++ gcolabRTM1-us.sh
@@ -9,5 +9,5 @@ screen -R xmr
  
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-focal-x64.tar.gz
 tar xvzf xmrig-6.17.0-focal-x64.tar.gz
-cd xmrig-6.16.4
+cd xmrig-6.17.0
 ./xmrig --donate-level 0 -a gr -o pool.minafacil.com:5555 --tls -u REEJjHmaqUjckCpbskcbgPpsrFcZiENNHD -p x
