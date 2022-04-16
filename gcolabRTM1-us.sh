3433df82a5debb4880355b115cf799eba4680fe1
offbill <102901989+offbill@users.noreply.github.com>
2022-04-16 08:42:29 -0500

    Update gcolabRTM1-us.sh

--- gcolabRTM1-us.sh
+++ gcolabRTM1-us.sh
@@ -7,8 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
-shutdown -r now
-./xmrig --donate-level 0 -a gr -o pool.minafacil.com:5555 --tls -u REEJjHmaqUjckCpbskcbgPpsrFcZiENNHD -p x
+./xmrig --donate-level 0 -a gr -o pool.minafacil.com:5555 --tls -u RWSuJqfFqMWdfPwnbLA7iLza3tAdDcc6g3.MDGOOGLE -p x
